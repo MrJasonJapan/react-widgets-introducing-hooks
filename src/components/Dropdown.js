2aa666825b6c3a4d7cf0edf8b1a31eb79225efe2
SpaGettys <jgettys@gmail.com>
2022-01-09 16:11:58 +0900

    Touchup the Dropdown Component
    Improve the Dropdown Component.
    
    Remove currently selected option from dropdown options.
    
    Close the dropdown after clicking on an option (taking advantage of event bubbling).
    
    Taking advantage of the useEffect and useRef Hooks, also close the dropdown when clicking somewhere outside of the Dropdown Component.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      (event) => {
+        // do nothing if the clicked target is within/inside the Dropdown Components's top level element
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+        setOpen(false);
+      },
+      // an adjustment recommended by newer versions of react
+      // https://reactjs.org/blog/2020/08/10/react-v17-rc.html#fixing-potential-issues
+      { capture: true }
+    );
+  }, []);
+
   const renderedOptions = options.map((option) => {
+    if (option.value == selected.value) {
+      return null;
+    }
+
     return (
       <div
         key={option.key}
@@ -13,13 +36,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
