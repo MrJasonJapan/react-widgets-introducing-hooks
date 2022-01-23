eda192967bd833ca2faaaea4c918e37495d8a737
SpaGettys <jgettys@gmail.com>
2022-01-23 17:57:41 +0900

    Start implementing the Translate Component,
    Start implementing the Translate Component, which will use our already existing Dropdown Component (making a few minor adjustments) and also use a new Convert Component we will focus on a little bit later. Then start working on the Convert Component. Pass in the ‘language’ and ‘text’ props. Take advantage of the useEffect hook to run code when ‘language’ or ‘text’ changes. Make a test call to the Google API based on the ‘language’ and ‘text’ props.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
@@ -40,14 +40,14 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         onClick={() => onSelectedChange(option)}
         className="item"
       >
-        {option.value}
+        {option.label}
       </div>
     );
   });
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div
           onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
