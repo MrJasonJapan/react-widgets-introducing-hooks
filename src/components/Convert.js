f2926ef609aff505d56827d5e2d3dacda160348f
SpaGettys <jgettys@gmail.com>
2022-01-23 18:18:57 +0900

    Work more on the Convert Component to finish up the Translation Widget.
    Adjust our Convert Component so we can place the Google API response into a variable, then set the state in charge of holding the translated text. Then show the translation on the screen. Also, Reduce the number of requests to the API. That will wrap up our Translation Widget.

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,21 +2,45 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
-  useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
-  }, [language, text]);
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
 
-  return <div />;
+  // useEffect #1 -> The timer will be cancelled if the user types in another character within 500ms.
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  // useEffect #2
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
